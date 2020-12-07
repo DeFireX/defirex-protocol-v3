4cd515f8c1b78485858b648b2129e412730a7f68
Dmitx <37021407+Dmitx@users.noreply.github.com>
2020-12-07 13:18:32 +0300

    Added interfaces for DfInfo & IDfTokenizedDeposit

---
+++ contracts/interfaces/IDfTokenizedDeposit.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.16;
+
+import "./IERC20.sol";
+
+interface IDfTokenizedDeposit {
+    function token() external returns (IERC20);
+    function dfWallet() external returns (address);
+
+    function tokenETH() external returns (IERC20);
+    function tokenUSDC() external returns (IERC20);
+
+    function fundsUnwinded(address) external returns (uint256);
+}
