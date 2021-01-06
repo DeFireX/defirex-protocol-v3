8692f6441ae8cc24041b129f728deb2e88d0dab5
Dmitx <37021407+Dmitx@users.noreply.github.com>
2021-01-06 18:00:49 +0300

    Remove TODO in ConstantDfWallet

--- contracts/constants/ConstantDfWalletMainnet.sol
+++ contracts/constants/ConstantDfWalletMainnet.sol
@@ -7,5 +7,5 @@ contract ConstantDfWallet {
     address public constant COMPTROLLER = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
     address public constant COMP_ADDRESS = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
 
-    address public constant DF_FINANCE_CONTROLLER = address(0xFff9D7b0B6312ead0a1A993BF32f373449006F2F); // TODO:
+    address public constant DF_FINANCE_CONTROLLER = address(0xFff9D7b0B6312ead0a1A993BF32f373449006F2F);
 }
